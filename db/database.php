76961e693139eee80a959f60ccec947c288ae915
Spud97 <luca.dercole97@gmail.com>
2022-07-12 16:12:08 +0200

    creato database

---
+++ db/database.php
@@ -0,0 +1,63 @@
+<?php
+$dischi = [
+    [
+        "src" => "../img/img-1.png",
+        "titolo" => "Appetite for Destruction",
+        "artista" => "Guns'n'Roses",
+        "anno" => 1987
+    ],
+    [
+        "src" => "../img/img-2.png",
+        "titolo" => "By the Way",
+        "artista" => "Red Hot Chili Peppers",
+        "anno" => 2002
+    ],
+    [
+        "src" => "../img/img-3.png",
+        "titolo" => "Queen II",
+        "artista" => "Queen",
+        "anno" => 1974
+    ],
+    [
+        "src" => "../img/img-4.png",
+        "titolo" => "Permanent Vacation",
+        "artista" => "Aerosmith",
+        "anno" => 1987
+    ],
+    [
+        "src" => "../img/img-5.png",
+        "titolo" => "Master of Puppets",
+        "artista" => "Metallica",
+        "anno" => 1985
+    ],
+    [
+        "src" => "../img/img-6.png",
+        "titolo" => "The Blues Brothers: Original Soundtrack Recording",
+        "artista" => "The Blues Brothers",
+        "anno" => 1980
+    ],
+    [
+        "src" => "../img/img-7.png",
+        "titolo" => "Killers",
+        "artista" => "Iron Maiden",
+        "anno" => 1981
+    ],
+    [
+        "src" => "../img/img-8.png",
+        "titolo" => "Demon Days",
+        "artista" => "Gorillaz",
+        "anno" => 2005
+    ],
+    [
+        "src" => "../img/img-9.png",
+        "titolo" => "Highway to Hell",
+        "artista" => "AC/DC",
+        "anno" => 1979
+    ],
+    [
+        "src" => "../img/img-10.png",
+        "titolo" => "Back to Black",
+        "artista" => "Amy Winehouse",
+        "anno" => 2006
+    ],
+];
